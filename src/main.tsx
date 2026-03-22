97727e595cd6b9a2ff6cc2beaf2f046b44cb141d
abdalrahimmakkawi <abdalrahimmakkawi@gmail.com>
2026-03-23 00:20:53 +0800

    force PWA update v4

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,14 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Auto-update service worker
+// Force update all PWA installations
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(reg => {
-    reg.addEventListener('updatefound', () => {
-      const newWorker = reg.installing;
-      if (newWorker) {
-        newWorker.addEventListener('statechange', () => {
-          if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-            newWorker.postMessage('skipWaiting');
-            window.location.reload();
-          }
-        });
-      }
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      registration.unregister();
     });
+  }).then(() => {
+    navigator.serviceWorker.register('/sw.js');
   });
 }
 
